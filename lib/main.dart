366772fdae5daab812b3dfc06d953690be7c0815
fredmessias43 <fredmessias43@gmail.com>
2022-02-28 23:29:23 -0300

    fix list row and add delete and done features

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -29,20 +31,62 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  List<String> itemsList = [];
+class Item {
+  int id;
+  String label;
+  bool done;
 
+  Item(this.id, this.label, this.done);
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Random rng = Random();
+  List<Item> itemsList = [];
   final myController = TextEditingController();
 
   final _formKey = GlobalKey<FormState>();
 
+  void fillItemsList() {
+    for (var i = 0; i < 1000; i++) {
+      int rand = rng.nextInt(9999);
+      setState(() {
+        itemsList.add(Item(rand, rand.toString(), false));
+      });
+    }
+  }
+
+  Color getColor(index) {
+    var colors = Colors.accents;
+
+    int color = index;
+
+    double calc = color / colors.length;
+
+    color = color - (calc.floor() * colors.length);
+
+    return colors[color];
+  }
+
   void addItemToList(value) {
-    itemsList.add(value);
+    itemsList.add(Item(rng.nextInt(9999), value, false));
+  }
+
+  void removeItemFromList(int id) {
+    itemsList.removeWhere((item) => item.id == id);
+  }
+
+  void toggleItemAsDone(int id) {
+    final int index = itemsList.indexWhere((item) => item.id == id);
+
+    itemsList[index].done = !itemsList[index].done;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+          onPressed: () => fillItemsList(),
+          child: const Icon(Icons.format_color_fill)),
       appBar: AppBar(
         title: Text(widget.title),
       ),
@@ -88,19 +132,55 @@ class _MyHomePageState extends State<MyHomePage> {
                   child: SizedBox(
                 height: 100.0,
                 child: ListView.builder(
-                  shrinkWrap: true,
-                  reverse: true,
-                  scrollDirection: Axis.horizontal,
+                  //reverse: true,
+                  //cacheExtent: 15,
+                  //addAutomaticKeepAlives: false,
+
+                  scrollDirection: Axis.vertical,
                   itemCount: itemsList.length,
                   itemBuilder: (item, index) {
-                    final item = itemsList[index];
+                    final item = itemsList.reversed.toList()[index];
 
-                    return Row(
-                      children: [
-                        ListTile(title: Text(item)),
-                        IconButton(
-                            onPressed: () {}, icon: const Icon(Icons.delete))
-                      ],
+                    return Container(
+                      height: 50,
+                      color: getColor(index),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 16, right: 16),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              item.label,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                  decoration: item.done
+                                      ? TextDecoration.lineThrough
+                                      : TextDecoration.none,
+                                  decorationThickness: 3.0),
+                            ),
+                            Row(
+                              children: [
+                                IconButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        toggleItemAsDone(item.id);
+                                      });
+                                    },
+                                    icon: Icon(
+                                        !item.done ? Icons.done : Icons.close)),
+                                IconButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        removeItemFromList(item.id);
+                                      });
+                                    },
+                                    icon: const Icon(Icons.delete)),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
                     );
                   },
                 ),
